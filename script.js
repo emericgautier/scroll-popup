4c1d6ba9724976dc2c61a006c58fb4374f9e1e90
emericgautier <emericgtr@gmail.com>
2022-07-27 18:58:25 +0200

    navbar effect

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+window.addEventListener("scroll", () => {
+  // Navbar effect
+  if (window.scrollY > 50) {
+    navbar.style.height = "45px";
+  } else {
+    navbar.style.height = "90px";
+  }
+});
+
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+// Réduire la navbar quand on descend vers le bas, la remettre à sa taille initiale quand on remonte tout en haut
 
 // Faire apparaitre l'image de la partie improvise
 
 // Faire apparaitre la popup quand on est en bas du site
 
-// Bonus : quand on clicke sur la popup elle disparait pour toujours
+// Bonus : quand on clique sur la pop-up elle disparait pour toujours
